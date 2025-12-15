c9b11eb690dfa9052cbd5e62090f165fb88cd109
Prince Koundal <princekoundal44@gmail.com>
2025-12-15 13:02:17 +0530

    added

--- feedback_system/settings.py
+++ feedback_system/settings.py
@@ -22,12 +22,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("SECRET_KEY", "unsafe-default-for-dev")
+SECRET_KEY = os.environ.get("SECRET_KEY", "unsafe-secret-key")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG", "False") == "True"
 
-ALLOWED_HOSTS = ["RIMT_Feedback.onrender.com", "localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["*", ".onrender.com"]
 
 
 # Application definition
@@ -81,7 +81,11 @@ WSGI_APPLICATION = 'feedback_system.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    "default": dj_database_url.config(default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}")
+    "default": dj_database_url.config(
+        default=os.environ.get("DATABASE_URL"),
+        conn_max_age=600,
+        ssl_require=True
+    )
 }
 
 
